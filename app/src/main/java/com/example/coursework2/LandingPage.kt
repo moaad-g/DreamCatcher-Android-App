64c93552b56d9bd1b354baafe65083e45651794c
Moaad <Mgountetti27@outlook.com>
2024-03-02 21:51:46 +0000

    changes

---
+++ app/src/main/java/com/example/coursework2/LandingPage.kt
@@ -0,0 +1,12 @@
+package com.example.coursework2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LandingPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.landing_page)
+    }
+}
