1e8f4f95da6ad39578b19164ca8e752461977729
Moaad <Mgountetti27@outlook.com>
2024-03-02 16:04:50 +0000

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Coursework2"
+include(":app")
+ 
