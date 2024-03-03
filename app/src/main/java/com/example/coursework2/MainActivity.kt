2bfb2f318dc2d417d8e11cf26678a62a7a197cd5
Moaad <Mgountetti27@outlook.com>
2024-03-03 02:39:20 +0000

    2 layouts done

--- app/src/main/java/com/example/coursework2/MainActivity.kt
+++ app/src/main/java/com/example/coursework2/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.coursework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.add_previous_night)
+        setSupportActionBar(findViewById(R.id.app_bar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return true
     }
 }
+
+
